c2775908025a75eb91a48d2c5be2621a77d25456
Harry <hgrenier@uri.edu>
2023-10-21 22:59:25 -0400

    fixed watch?

--- 412/Prog04/Scripts/watch.sh
+++ 412/Prog04/Scripts/watch.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <watch_folder> <version> <output_folder>"
+    exit 1
+fi
+
 # Compile the executables
 gcc version1.c -o version1
 gcc version2.c -o version2
 gcc version3.c -o version3
 
+# Arguments
 WATCH_FOLDER="$1"
 VERSION="$2"
 OUTPUT_FOLDER="$3"
-PROCESSED_FILES="processed_files.tmp"
+PROCESSED_FILES=".processed_files.tmp"
 
+# Create the watch folder if it doesn't exist
 mkdir -p "$WATCH_FOLDER"
-touch "$PROCESSED_FILES"
 
-while true; do
-    # Process newly added .dat files or folders
-    find "$WATCH_FOLDER" -type f -name "*.dat" -o -type d | while read entry; do
-        # If not already processed
-        if ! grep -q "^$entry$" "$PROCESSED_FILES"; then
-            if [[ "$entry" == *.dat ]]; then
-                ./version"$VERSION" "$OUTPUT_FOLDER" "$entry"
-                echo "$entry" >> "$PROCESSED_FILES"
-            else  # It's a folder
-                # Find all .dat files in the folder
-                dat_files=($(find "$entry" -type f -name "*.dat"))
-                if (( ${#dat_files[@]} > 0 )); then
-                    ./version"$VERSION" "$OUTPUT_FOLDER" "${dat_files[@]}"
-                    # Mark all processed .dat files in the folder
-                    for file in "${dat_files[@]}"; do
-                        echo "$file" >> "$PROCESSED_FILES"
-                    done
-                fi
-                # Mark folder as processed to avoid re-checking in the future
-                echo "$entry" >> "$PROCESSED_FILES"
-            fi
+# Create or clear the processed files list
+> "$PROCESSED_FILES"
+
+# Monitor the watch folder indefinitely
+inotifywait -m -e create -e moved_to --format "%f" "$WATCH_FOLDER" | while read ENTRY; do
+    ENTRY_PATH="$WATCH_FOLDER/$ENTRY"
+    
+    if [[ "$ENTRY" == *.dat ]]; then
+        # Execute the command on the new .dat file
+        ./version"$VERSION" "$OUTPUT_FOLDER" "$ENTRY_PATH"
+        echo "$ENTRY_PATH" >> "$PROCESSED_FILES"
+    elif [ -d "$ENTRY_PATH" ]; then
+        # Find all .dat files in the new directory
+        dat_files=($(find "$ENTRY_PATH" -type f -name "*.dat"))
+        if (( ${#dat_files[@]} > 0 )); then
+            ./version"$VERSION" "$OUTPUT_FOLDER" "${dat_files[@]}"
+            for file in "${dat_files[@]}"; do
+                echo "$file" >> "$PROCESSED_FILES"
+            done
         fi
-    done
-    sleep 5
+    fi
 done
